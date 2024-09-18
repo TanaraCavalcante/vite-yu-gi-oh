9de735d73042e2572c0fa18fdfc62b0c3df532b4
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-09-18 19:11:46 -0400

    initial tests

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        data() {
+            return{
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <h1>Header</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>

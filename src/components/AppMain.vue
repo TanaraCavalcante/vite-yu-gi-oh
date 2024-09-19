ecedfb356c4cbbc5845c7f91bbbe9edbfd5f7a17
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-09-19 16:45:24 -0400

    added components - Main parent

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,28 @@
 <script>
+import MainList from './MainList.vue';
+import MainListItem from './MainListItem.vue';
+
   export default {
     data() {
         return{
 
         }
+    },
+    components:{
+        MainList,
+        MainListItem
     }
 }
 </script>
 
 <template>
+   
     <div>
         <h1>Main</h1>
     </div>
+
+    <MainList/>
+    <MainListItem/>
 </template>
 
 <style scoped>
---
+++ src/components/MainListItem.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    data() {
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h3>Main list item</h3>
+    </div>
+</template>
+
+<style scoped>
+ 
+</style>

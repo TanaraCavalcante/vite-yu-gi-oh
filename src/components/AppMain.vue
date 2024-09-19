094a9e3da0085038d5ddb222dad4b7dada81849f
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-09-19 19:00:52 -0400

    import Axios

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,31 @@
 <script>
 import MainList from './MainList.vue';
+import axios from "axios";
 
   export default {
     data() {
         return{
-
+            cardList: [],
         }
     },
     components:{
         MainList,
+    },
+    methods:{
+        getCardList(){
+            axios.get('/user?ID=12345')
+            .then(function (response) {
+             // handle success
+             console.log(response);
+            })
+            .catch(function (error) {
+             // handle error
+             console.log(error);
+             })
+            .finally(function () {
+              // always executed
+            });
+         }
     }
 }
 </script>
